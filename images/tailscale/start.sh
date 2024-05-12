4628c898cb680939a52056cc6c4220dfc5e1969e
Arnab Mukherjee <35556016+mukherjeearnab@users.noreply.github.com>
2024-05-12 15:55:13 +0530

    Added Exit Node config

--- images/tailscale/start.sh
+++ images/tailscale/start.sh
@@ -4,7 +4,7 @@ echo "Starting Tailscale daemon"
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
 tailscaled --tun=userspace-networking --state=${TAILSCALE_STATE_ARG} ${TAILSCALE_OPT} &
 PID=$!
-until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
+until tailscale up --advertise-exit-node --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
     sleep 0.1
 done
 tailscale status
